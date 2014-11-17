ab6a9b111de3674e4d55fbae8376dd23f07b00ea
taylor.ledingham <taylor.ledingham@gmail.com>
2014-11-17 12:28:04 -0800

    Initial Commit

---
+++ DJFit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DJFit
+//
+//  Created by Taylor Ledingham on 2014-11-17.
+//  Copyright (c) 2014 Taylor Ledingham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
