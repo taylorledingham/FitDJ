6d971b6af18dea151163639fa284e91e00b024a2
taylor.ledingham <taylor.ledingham@gmail.com>
2014-11-24 12:24:27 -0800

    added watch kit extension

---
+++ DJFit WatchKit Extension/NotificationController.h
@@ -0,0 +1,14 @@
+//
+//  NotificationController.h
+//  DJFit WatchKit Extension
+//
+//  Created by Taylor Ledingham on 2014-11-24.
+//  Copyright (c) 2014 Taylor Ledingham. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface NotificationController : WKUserNotificationInterfaceController
+
+@end
